47deb93140e6b9eef9feac6a4107ed30418ddc37
MaG-TeC <gungor.mahmet@gmail.com>
2020-01-05 21:29:22 +0100

    XML Smoke Test

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -10,10 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 public class TestBase {
@@ -21,9 +19,10 @@ public class TestBase {
     protected WebDriver driver;
     protected Actions action;
     protected WebDriverWait wait;
-    protected ExtentReports report;
-    protected ExtentHtmlReporter htmlReporter;
-    protected ExtentTest extentLogger;
+    protected static ExtentReports report;
+    protected static ExtentHtmlReporter htmlReporter;
+    protected static ExtentTest extentLogger;
+    protected String url;
 
     @BeforeTest
     public void setUpTest(){
@@ -52,12 +51,25 @@ public class TestBase {
         report.flush();
     }
     @BeforeMethod
-    public void setUpMethod(){
+    @Parameters("env")
+    public void setUpMethod(@Optional String env){
+        System.out.println("env = " + env);
+
+        //if env variable is null use default url
+        //if it is not null, get url based on env
+        if(env==null) {
+            url=ConfigurationReader.get("url");
+        }else{
+         url=ConfigurationReader.get(env + "_url");
+        }
+
         driver = Driver.get();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         action = new Actions(driver);
         wait = new WebDriverWait(driver,10);
-        driver.get(ConfigurationReader.get("url"));
+
+        driver.get(url);
+
         driver.manage().window().maximize();
     }
     //ITestResult class describes the result of a test in TestNg
